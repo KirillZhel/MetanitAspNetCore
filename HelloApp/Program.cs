6dfb912fcff94bf85c982cd4ae902463ab310aee
Кирилл Желтышев <zheltyshev.kirill@gmail.com>
2022-11-01 02:04:54 +0300

    прочитал до "глава 2, Метод Run и определение терминального middleware"

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+RequestDelegate request = async (context) => await context.Response.WriteAsync("Hello METANIT.COM");
 
+int x = 2;
+app.Run(async (context) =>
+{
+	x = x * 2;  //  2 * 2 = 4
+	await context.Response.WriteAsync($"Result: {x}");
+});
+
+//app.UseWelcomePage();   // подключение WelcomePageMiddleware
+//app.Run(request);
 app.Run();
+
+
+//app.MapGet("/", () => "Hello World!");
+//app.MapGet("/lox", () => "ты лох!");
+//app.UseWelcomePage();
+
+
+//app.Start();
+//await Task.Delay(10000);
+//await app.StopAsync();
